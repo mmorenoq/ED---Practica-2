13411ecd4f6254c8936559fe6edd23203a12b287
Miguel Moreno <mmorenoher@gmail.com>
2020-04-24 20:30:32 -0700

    Add project files.

---
+++ ED - Practica 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED___Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombrePersonas = new string [10];
+
+            for (int i = 0; i < nombrePersonas.Length; i++)
+            {
+                Console.Write("Introduzca el nombre de la persona " + (i + 1) + ":");
+                nombrePersonas[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=======================================");
+            Console.WriteLine();
+            Console.WriteLine("Los nombres de personas ingresadas son:");
+
+            foreach (string i in nombrePersonas)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
